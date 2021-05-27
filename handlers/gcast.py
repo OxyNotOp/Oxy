0daa7201f49510d75160c040d297cd544c6ad60c
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-27 22:54:48 +0530

    update gcast.py

--- handlers/gcast.py
+++ handlers/gcast.py
@@ -1,3 +1,20 @@
+# OxyXmusic (Telegram bot project )
+# Copyright (C) 2021  OxyNotOp
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
 import asyncio
@@ -7,9 +24,10 @@ from config import SUDO_USERS
 async def bye(client, message):
     sent=0
     failed=0
+    lol = await message.reply("Starting Gcast")
     if message.from_user.id in SUDO_USERS:
-        if not message.reply_to_message.text:
-            await message.reply("Reply to any text message to gcast sir")
+        if not message.reply_to_message:
+            await lol.edit("Reply to any text message to gcast sir")
             return
         msg = message.reply_to_message.text
         for dialog in client.iter_dialogs():
