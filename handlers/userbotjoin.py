e35d2d191830d817e8ba82dc3ff797b4fd9e750e
Your Daddy <71930942+OxyNotOp@users.noreply.github.com>
2021-05-27 23:17:13 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -53,7 +53,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @Oxy_Assistant to your Group and try again</b>",
+            "\n\nOr manually add @Oxy_VC_2 to your Group and try again</b>",
         )
         return
     await message.reply_text(
